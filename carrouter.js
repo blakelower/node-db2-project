dbb46f4d23f8e53c9ebc2ce4d8269cbe7fd55679
blakelower <blakelower1999@gmail.com>
2020-04-15 15:11:34 -0700

    update

--- carrouter.js
+++ carrouter.js
@@ -1,19 +1,43 @@
-const express = require("express");
-const knex = require("knex");
-
-const knexConfig = require("./knexfile");
-
-const db = knex(knexConfig.development);
+const express = require('express');
+const db = require('./migrations/dbconfig');
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  try {
-    const cars = await db("cars");
+router.get('/', (req, res) => {
+  db('cars')
+  .then(cars => {
+    res.json(cars); 
+  })
+  .catch (err => {
+    res.status(500).json({ message: 'Failed to retrieve cars' });
+  });
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  db('cars').where({ id }).first()
+  .then(cars => {
     res.json(cars);
-  } catch (err) {
-    res.status(500).json({ message: "retrieval failure" });
-  }
+  }) 
+  .catch (err => {
+    res.status(500).json({ message: 'Failed to retrieve car' });
+  });
+});
+
+router.post('/', (req, res) => {
+  const carData = req.body;
+  db('cars').insert(carData)
+  .then(ids => {
+    db('cars').where({ id: ids[0] })
+    .then(newCarEntry => {
+      res.status(201).json(newCarEntry);
+    });
+  })
+  .catch (err => {
+    console.log('POST error', err);
+    res.status(500).json({ message: "Failed to store data" });
+  });
 });
 
 module.exports = router;
--- migrations/20200414132102_create-cars-table.js
+++ migrations/20200414132102_create-cars-table.js
@@ -1,14 +1,15 @@
-
-exports.up = function(knex) {
-  return knex.schema.createTable('users', tbl => {
-      tbl.increment();
-      tbl.text('car', 128)
-      .unique()
-      .notNullable();
-      tbl.text('dealership');
+exports.up = function (knex) {
+  return knex.schema.createTable("cars", (tbl) => {
+    tbl.increments();
+    tbl.string("vin", 17).unique().notNullable();
+    tbl.string("make", 40).notNullable();
+    tbl.string("model", 40).notNullable();
+    tbl.integer("mileage", 7).notNullable();
+    tbl.string("transmissionType");
+    tbl.string("title");
   });
 };
 
-exports.down = function(knex) {
-  return knex.schema.dropTableIfExists('users');
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("cars");
 };
