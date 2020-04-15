9724575ce4011867aaef3c12c93f1724d0983b8b
blakelower <blakelower1999@gmail.com>
2020-04-15 14:33:13 -0700

    update

--- api/server.js
+++ api/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const helmet = require('helmet');
 
-// const fruitsRouter = require('../fruits/fruits-router.js');
+const carsRouter = require('../carrouter');
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 
-// server.use('/api/cars', fruitsRouter);
+server.use('/api/cars', carsRouter);
 
 module.exports = server;
---
+++ carrouter.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const knex = require("knex");
+
+const knexConfig = require("./knexfile");
+
+const db = knex(knexConfig.development);
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const cars = await db("cars");
+    res.json(cars);
+  } catch (err) {
+    res.status(500).json({ message: "retrieval failure" });
+  }
+});
+
+module.exports = router;
